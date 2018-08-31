ece77c62afa3f78b3b4c7038a29250d6f11b57aa
baoshengzhu <153620863@qq.com>
2018-08-31 11:17:39 +0800

    init

---
+++ script/app_monitor.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+#-*- coding: utf8 -*-
+
+# logging
+#
+
+import logging
+from logging.handlers import RotatingFileHandler
+import subprocess
+
+from  psutil import Process
+import psutil
+import argparse
+import os
+import sys
+import time
+import socket
+import json
+import traceback
+
+from kafka import KafkaProducer
+
+
+## log settings###### 
+LOG_PATH_FILE = "./app_monitor_mgr.log"
+LOG_MODE = 'a'
+LOG_MAX_SIZE = 4*1024*1024 # 4M per file
+LOG_MAX_FILES = 4          # 4 Files: app_monitor_mgr.log.1, app_monitor_mgr.2, ...
+LOG_LEVEL = logging.ERROR
+LOG_FORMAT = "%(asctime)s %(levelname)-10s[%(filename)s:%(lineno)d(%(funcName)s)] %(message)s"
+
+handler = RotatingFileHandler(LOG_PATH_FILE, LOG_MODE, LOG_MAX_SIZE, LOG_MAX_FILES)
+formatter = logging.Formatter(LOG_FORMAT)
+handler.setFormatter(formatter)
+
+Logger = logging.getLogger()
+Logger.setLevel(LOG_LEVEL)
+Logger.addHandler(handler)
+
+def send_to_kafka(topic,message):
+    try:
+        #print topic,message
+        Logger.info(topic,message)
+        message=json.dumps(message)
+        result =producer.send(topic,message)
+    except:
+        logging.error('Error create producer in kafka: {}'.format(traceback.format_exc()))
+
+def get_host_ip():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        ip = s.getsockname()[0]
+    finally:
+        s.close()
+    return ip
+
+def _argparse():
+    """
+    argument parser
+    """
+    parser = argparse.ArgumentParser(description='health checker for application')
+    parser.add_argument('--type', action='store', dest='type', required=True,
+                        help='Monitoring type')
+    return parser.parse_args()
+
+def command(monitor_type):
+    cmd = "ps -eaf|grep %s|grep -v grep |grep -v 'yy.com'|awk '{print $2}'"%(monitor_type)
+    return cmd
+
+def collect_status(pid):
+    ip=get_host_ip()
+    physical_mem=psutil.virtual_memory().total
+    cpu_count=psutil.cpu_count()
+    its=int(time.time())
+    p_ins=Process(pid)
+    pstatus=p_ins.status()
+    memory_percent=p_ins.memory_percent()
+    memory_used=memory_percent*physical_mem
+    cpu_percent=p_ins.cpu_percent()
+    num_fds=p_ins.num_fds()
+    connections=p_ins.connections()
+    connections_num=len(connections)
+    #appname=p_ins.cwd()
+    if p_ins.name() == 'jsvc':
+        appname=p_ins.exe()
+    else:
+        appname=p_ins.cwd()
+    #print p_ins.name()
+    #print appname
+    if p_ins.children(recursive=True):
+        children_list = str(p_ins.children(recursive=True))
+    else:
+		children_list = None
+    message = {'ip':ip,'pstatus':pstatus,'metric_name':'app_monitor','its':its,'pid':pid,'physical_mem':physical_mem,'memory_used':memory_used,'memory_percent':memory_percent,'cpu_count':cpu_count,'cpu_percent':cpu_percent,'num_fds':num_fds,'connections_num':connections_num,'appname':appname,'children':children_list}
+    return message 
+
+if __name__ == '__main__':
+    parser = _argparse()
+    cmd=command(parser.type)
+    if cmd is None:
+        sys.exit()
+
+    ##kafka的信息###
+    kafka_host='kafka4metric.huya.com'
+    kafka_port=7619
+    topic='ops_metric'
+    #kafka_host='10.64.42.217'
+    #kafka_port=9092
+    #topic='test-user-event-data'
+
+    try:
+        producer = KafkaProducer(bootstrap_servers=['{kafka_host}:{kafka_port}'.format(kafka_host=kafka_host,kafka_port=kafka_port)])
+    except:
+        logging.error('Error create producer in kafka: {}'.format(traceback.format_exc()))
+
+    p = psutil.subprocess.Popen(cmd,shell=True,stdout=psutil.subprocess.PIPE)
+    java_pid_list = p.stdout.readlines()
+    if not java_pid_list:
+        sys.exit(0)
+    #print java_pid_list
+    for pid in java_pid_list:
+        test_pid = int(pid.strip())
+        message=collect_status(test_pid)
+        try:
+            send_to_kafka(topic,message)
+        except Exception as e:
+            print e
+    producer.flush()
+
+
